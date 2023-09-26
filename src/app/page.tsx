346b8325b36c244b1dc32bc33b4afdd328c59842
Minh Le <minh.le@ledminh.dev>
2023-09-25 23:50:29 -0400

    Introbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
+import IntroBox from "@/components/home/IntroBox";
 import { mainMenuData } from "@/constants";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen text-center">
-        <p className="flex flex-col mt-4 text-2xl">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-800 to-orange-950">
-            Hello, I'm Minh Le.
-          </span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-800 to-orange-950">
-            I'm a software engineer.
-          </span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-800 to-orange-950">
-            I'm proficient in ReactJS, NextJS, and TypeScript. I'm also a fan of
-            TailwindCSS.
-          </span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-800 to-orange-950">
-            I'm currently learning AWS and database.
-          </span>
-        </p>
-        <ul className="flex items-center justify-between gap-4">
-          {mainMenuData.map((item) => (
-            <li>
-              <Link href={item.link}>{item.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <Wrapper>
+      <IntroBox />
+      <ul className="flex items-center justify-between gap-4">
+        {mainMenuData.map((item) => (
+          <li key={item.id}>
+            <Link href={item.link}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Wrapper>
   );
 }
+
+/*********************
+ * Styles
+ */
+
+const Wrapper = (props: { children: React.ReactNode }) => (
+  <div className="flex flex-col items-center justify-center min-h-screen text-center">
+    {props.children}
+  </div>
+);
---
+++ src/components/home/IntroBox.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+
+export default function IntroBox() {
+  return (
+    <Wrapper>
+      <Text>
+        <Line>
+          <h1 className="text-5xl">
+            Hello, I'm <Emphasize>Minh Le</Emphasize>. I'm a web developer.
+          </h1>
+        </Line>
+        <Line>------------</Line>
+        <Line>
+          I'm proficient in <Emphasize>ReactJS</Emphasize>,{" "}
+          <Emphasize>NextJS</Emphasize>, and <Emphasize>TypeScript</Emphasize>.
+          I'm also a fan of <Emphasize>TailwindCSS</Emphasize>.
+        </Line>
+        <Line>
+          I'm currently learning <Emphasize>AWS</Emphasize> and{" "}
+          <Emphasize>database</Emphasize>.
+        </Line>
+      </Text>
+      <LearnMoreLink />
+    </Wrapper>
+  );
+}
+
+/***********************
+ * Styles
+ */
+const Wrapper = (props: { children: React.ReactNode }) => (
+  <div className="relative p-8 mx-4 overflow-hidden border-4 border-gray-500">
+    {props.children}
+  </div>
+);
+
+const Text = (props: { children: React.ReactNode }) => (
+  <ul className="flex flex-col gap-4 mb-4 font-mono text-2xl">
+    {props.children}
+  </ul>
+);
+
+const Line = (props: { children: React.ReactNode }) => (
+  <li className="text-transparent bg-clip-text bg-gradient-to-r from-orange-800 to-orange-950">
+    {props.children}
+  </li>
+);
+
+const Emphasize = (props: { children: React.ReactNode }) => (
+  <span className="font-extrabold text-red-700">{props.children}</span>
+);
+
+const LearnMoreLink = () => (
+  <Link
+    href="/about"
+    className="absolute inline-block px-4 py-2 mt-4 text-lg font-bold text-white bg-gray-500 rounded-tl-lg -right-1 -bottom-1 hover:text-gray-300"
+  >
+    Learn more about me
+  </Link>
+);
