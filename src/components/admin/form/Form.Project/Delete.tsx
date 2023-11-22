b8137c7f04d5ce1ff309a8a2bfd5198714cd6295
Minh Le <duyminh.le310@gmail.com>
2023-11-22 12:04:16 -0500

    Delete

--- src/components/admin/form/Form.Project/Delete.tsx
+++ src/components/admin/form/Form.Project/Delete.tsx
@@ -9,15 +9,18 @@ export default function ProjectForm(props: {
   project: Project;
 }) {
   return (
-    <div>
-      <h2>Do you want to delete this project</h2>
-      <div className="flex gap-2">
-        <p>{props.project.title}</p>
-        <p>{props.project.description}</p>
-        <p>{props.project.order}</p>
-        <p>{props.project.github}</p>
-        <p>{props.project.demo}</p>
-        <div className="relative w-10 h-10">
+    <div className="flex flex-col gap-4">
+      <h2 className="font-semibold">Do you want to delete this project</h2>
+      <div className="flex gap-2 border border-red-600 p-2 flex-col rounded-md">
+        <h3 className="font-semibold border-b border-black">
+          {props.project.title}
+        </h3>
+        <p className="font-mono bg-slate-300 px-2">
+          {props.project.description.length > 20
+            ? props.project.description.slice(0, 20) + "..."
+            : props.project.description}
+        </p>
+        <div className="relative w-40 h-40 shadow-lg">
           <Image
             src={props.project.image.src}
             alt={props.project.image.alt}
@@ -28,7 +31,7 @@ export default function ProjectForm(props: {
       </div>
       <div className="flex gap-2">
         <button
-          className="btn btn-primary"
+          className="btn btn-attention"
           onClick={() => props.onSubmit(props.project.id)}
         >
           Yes
--- src/components/admin/form/Form.ProjectCategory/Delete.tsx
+++ src/components/admin/form/Form.ProjectCategory/Delete.tsx
@@ -11,9 +11,9 @@ export default function ProjectCategoryForm(props: {
     <div className="flex flex-col gap-4">
       <h2 className="font-semibold">Do you want to delete this category?</h2>
       <div className="flex gap-2 border border-red-600 p-2 flex-col rounded-md">
-        <p className="font-semibold border-b border-black">
+        <h3 className="font-semibold border-b border-black">
           {props.category.title}
-        </p>
+        </h3>
         <p className="font-mono bg-slate-300 px-2">
           {props.category.description.length > 20
             ? props.category.description.slice(0, 20) + "..."
