7a63b2f1b60658fd8c7ad7199bc3dd9f1fd30d9f
Minh Le <minh.le@ledminh.dev>
2024-01-15 17:01:50 -0500

    fix layout/projects

--- src/app/(admin_pages)/admin/projects/layout.tsx
+++ src/app/(admin_pages)/admin/projects/layout.tsx
@@ -1,10 +1,9 @@
 import { ReactNode } from "react";
-import { deleteImages } from "@/utils/deleteImages";
 
 export default function ProjectsAdminLayout(props: { children: ReactNode }) {
   return (
     <>
-      <h1 className="text-3xl mb-4 border-b-2 border-black">Projects</h1>
+      <h1 className="mb-4 text-3xl border-b-2 border-black">Projects</h1>
       <div>{props.children}</div>
     </>
   );
